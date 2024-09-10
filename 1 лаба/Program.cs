e6927573422636928f43e6f4bde67e4d755d170b
unknown <429196-25>
2024-09-10 11:56:11 +0300

    add project

---
+++ 1 лаба/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int s = 1;
+            int a, b, c, d;
+            Console.WriteLine("Введите 4 числа");
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            c = Convert.ToInt32(Console.ReadLine());
+            d = Convert.ToInt32(Console.ReadLine());
+            if ((a % 2 != 0) && (c % 2 != 0) && (b % 2 != 0) && (d % 2 != 0)) //все числа нечетные
+            {
+                s = a * b * c * d; //проиводение всех
+                Console.SetCursorPosition(40, 5);
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine(s);
+                Console.ResetColor();
+
+            }
+            else
+            {
+                if (a % 2 == 0) //проверка первого числа на четность
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow; //присваивание цвета
+
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;//присваивание цвета
+
+                }
+                Console.SetCursorPosition(40, 5); // вывод по центру
+                Console.WriteLine(a);
+                Console.ResetColor();
+                if (b % 2 == 0)//проверка второго числа на четность
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;//присваивание цвета
+
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;//присваивание цвета
+                }
+                Console.SetCursorPosition(40, 6); // вывод по центру
+                Console.WriteLine(b);
+                Console.ResetColor();
+                if (c % 2 == 0) //проверка третьего числа на четность
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;//присваивание цвета
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;//присваивание цвета
+                }
+                Console.SetCursorPosition(40, 7); // вывод по центру
+                Console.WriteLine(c);
+                Console.ResetColor();
+                if (d % 2 == 0) //проверка четвертого числа на четность
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;//присваивание цвета
+                }
+                else
+                {
+                    Console.SetCursorPosition(40, 5);// вывод по центру
+                    Console.ForegroundColor = ConsoleColor.Red;//присваивание цвета
+                }
+                Console.SetCursorPosition(40, 5);// вывод по центру
+                Console.WriteLine(d);
+                Console.ResetColor();
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
+
---
+++ 1 лаба/Program2.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace Lab2
+{
+    class Program
+    {
+        static void Main()
+        {
+            string s;
+            int attempts = 0;   //количество попыток 
+            int i = 0;
+            int kol;
+            char[] ran = {'а','б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и',
+            'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц',
+            'ч', 'ш', 'щ', 'ы', 'э', 'ю', 'я'};
+            int buf = 0;
+            Console.Write($"Введите слово состоящее из 4 букв и начинающиеся на {ran[buf]}:  ");// меняется заглавная буква
+            s = Console.ReadLine();
+            while (!String.IsNullOrEmpty(s)||(buf==31))
+            {
+                if ((s.Length == 4) && (s[0] == ran[buf]))// число должно быть 4 значным и начинающиеся на заглавные буквы которые указаны
+                {
+                    i++;//посчет праивльных ответов
+                }
+                attempts++;//кол-во попыток
+                buf++;//чередование букв по алфавиту
+                Console.Write($"Введите слово состоящее из 4 букв и начинающиеся на {ran[buf]}: ");
+                s = Console.ReadLine();
+            }
+            kol =  i * 100 / 31;//Количество баллов
+            Console.WriteLine("Количество попыток:" + attempts);
+            Console.WriteLine("Количество правильных слов: " + i);
+            Console.WriteLine("Количество баллов:" + kol);
+            Console.ReadLine();
+        }
+    }
+}
+
